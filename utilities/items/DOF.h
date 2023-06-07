949f2ad984b75408642070697c2d7aa887b95f33
papadeiv <davidepapapicco93@gmail.com>
2023-06-07 12:22:45 +0200

    Basic implementation of class DOF

---
+++ utilities/items/DOF.h
@@ -0,0 +1,25 @@
+// PRIVATE HEADER
+#pragma once
+
+// Base class header
+#include "../../src/geometry/Point.h"
+
+class DOF: public Point{
+public:
+	// Default constructor
+	DOF();
+	// Other constructors
+	// Destructor
+	~DOF() = default;
+
+	// Accessors
+	// Other methods
+
+protected:
+	// Attributes to be inherited
+	// Overloaded methods
+
+private:
+	// Attributes
+	// Private methods
+};
